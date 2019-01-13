d576cc71d02b49af621441848850a9306ee3b911
Carter Nesbitt <ccnesbitt@gmail.com>
2019-01-12 23:06:30 -0500

    Added reference to opcode functions. This commit produces errors. They will immediately be removed with the following commit. May go back to try and remedy these errors, which would provide for cleaner code.

--- include/cpu.h
+++ include/cpu.h
@@ -3,11 +3,14 @@
 
 #include <sys/types.h>
 
+typedef void (*FunctionExecute)(unsigned char, unsigned char);
 struct opcode {
   unsigned char code[3];
   char mode[11];
   u_int8_t operands;
-};
+  //void (*opFunction)(unsigned char, unsigned char);
+  void *functionExecute;
+} extern const opcodes[256];
 
 void step(void);
 
