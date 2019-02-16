3b17416ccf97763298ed62bf38656ec27531602c
Carter Nesbitt <ccnesbitt@gmail.com>
2019-02-16 14:45:39 -0500

    Set and clear functions for PPUMASK.

--- source/memoryMappedIO.c
+++ source/memoryMappedIO.c
@@ -54,6 +54,55 @@ void setNMIGeneration(uint8_t set) {
   register  at $2001 (Controller register)
 */
 
+void setGrayScale(uint8_t set) {
+  if (set) {
+    ppuRegisters.PPUMask |= 1;
+  } else ppuRegisters.PPUMask &= 0b11111110;
+}
+
+void setBackgroundLeftEightPixelsActive(uint8_t set) {
+  if (set) {
+    ppuRegisters.PPUMask |= (1 << 1);
+  } else ppuRegisters.PPUMask &= ~(1 << 1);
+}
+
+void setSpriteLeftEightPixelsActive(uint8_t set) {
+  if (set) {
+    ppuRegisters.PPUMask |= (1 << 2);
+  } else ppuRegisters.PPUMask &= ~(1 << 2);
+}
+
+void setBackground(uint8_t set) {
+  if (set) {
+    ppuRegisters.PPUMask |= (1 << 3);
+  } else ppuRegisters.PPUMask &= ~(1 << 3);
+}
+
+void setSprites(uint8_t set) {
+  if (set) {
+    ppuRegisters.PPUMask |= (1 << 4);
+  } else ppuRegisters.PPUMask &= ~(1 << 4);
+}
+
+void setEmphasizeRed(uint8_t set) {
+  if (set) {
+    ppuRegisters.PPUMask |= (1 << 5);
+  } else ppuRegisters.PPUMask &= ~(1 << 5);
+}
+
+void setEmphasizeGreen(uint8_t set) {
+  if (set) {
+    ppuRegisters.PPUMask |= (1 << 6);
+  } else ppuRegisters.PPUMask &= ~(1 << 6);
+}
+
+void setEmphasizeBlue(uint8_t set) {
+  if (set) {
+    ppuRegisters.PPUMask |= (1 << 7);
+  } else ppuRegisters.PPUMask &= ~(1 << 7);
+}
+
+
 /**
   The following functions clear/set bits of the memory mapped
   register  at $2002 (Controller register)
